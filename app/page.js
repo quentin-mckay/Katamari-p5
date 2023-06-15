19e979616f2cdf14b10b09c411ec7aa225a7353d
Quentin McKay <quentinmckay1@gmail.com>
2023-06-15 21:32:41 +1000

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Katamari from '@/components/Katamari'
+
+const Home = () => {
+    return <div className='bg-slate-400 max-h-screen text-3xl overflow-hidden'>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
+		<Katamari />
+	</div>
+}
+export default Home
