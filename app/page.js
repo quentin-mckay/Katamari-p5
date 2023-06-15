46e0872bb2fd3c11694f04fffee9dbe74327f40e
Quentin McKay <quentinmckay1@gmail.com>
2023-06-15 22:00:11 +1000

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Katamari from '@/components/Katamari'
 
 const Home = () => {
-    return <div className='bg-slate-400 max-h-screen text-3xl overflow-hidden'>
+    return <div className='bg-slate-800 max-h-screen text-3xl text-white overflow-hidden'>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, laudantium! Quasi asperiores iste vero sint eius, facere repudiandae! Distinctio libero unde eos voluptatum dolor rerum facilis. Eveniet, placeat, velit ut nihil molestias soluta odio corporis saepe excepturi, neque maxime quaerat magnam tenetur ipsum minus odit. Nulla vel amet numquam doloremque.</p>
